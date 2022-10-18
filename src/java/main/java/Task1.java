2301413d29b11e1ceec934a1c0ac5df4bfed3a6a
Junaid <Junaid.mah@hotmail.com>
2022-10-18 17:57:21 +0100

    Add main

--- src/java/main/java/Task1.java
+++ src/java/main/java/Task1.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Task1 {
+import java.util.List;
+
+public class Task1 {
+
+//    public void sorting(int[] list) {
+//        System.out.println(list);
+//    }
+
+    int sorting(int[] ints) {
+        return 1;
+    }
 }
