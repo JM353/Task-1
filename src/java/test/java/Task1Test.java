30a4e03bf73364cf6ad17006c360a03483f6d240
Junaid <Junaid.mah@hotmail.com>
2022-10-18 17:58:31 +0100

    Add test

--- src/java/test/java/Task1Test.java
+++ src/java/test/java/Task1Test.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Task1Test {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task1Test {
+    Task1 task1 = new Task1();
+
+
+    @Test
+    public void minimumValue() {
+        assertEquals(-2, task1.sorting(new int[] {6, 9, 15, -2, 92, 11}));
+    }
 }
