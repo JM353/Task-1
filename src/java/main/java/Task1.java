af6ffad4d0ec66ebbaeae87e3789dc4084e53567
Junaid <Junaid.mah@hotmail.com>
2022-10-19 01:13:13 +0100

    Max Value code added and passed

--- src/java/main/java/Task1.java
+++ src/java/main/java/Task1.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Task1 {
 
 
-    public int sorting(int[] ints) {
+    public int minVal(int[] ints) {
         int min = ints[0];
         for (int i = 1; i < ints.length; i++) {
             if (ints[i] < min) {
@@ -12,5 +12,15 @@ public class Task1 {
             }
         }
         return min;
+    }
+
+    public int maxVal(int[] ints) {
+        int max = ints[1];
+        for (int i = 1; i < ints.length; i++) {
+            if (ints[i] > max) {
+                max = ints[i];
+            }
+        }
+        return max;
     }
 }
