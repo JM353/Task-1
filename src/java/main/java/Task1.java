32496e2fe3a0dba12b3d92b4337618539588042f
Junaid <Junaid.mah@hotmail.com>
2022-10-19 01:06:10 +0100

    Min Value

--- src/java/main/java/Task1.java
+++ src/java/main/java/Task1.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Task1 {
 
-//    public void sorting(int[] list) {
-//        System.out.println(list);
-//    }
 
-    int sorting(int[] ints) {
-        return 1;
+    public int sorting(int[] ints) {
+        int min = ints[0];
+        for (int i = 1; i < ints.length; i++) {
+            if (ints[i] < min) {
+                min = ints[i];
+            }
+        }
+        return min;
     }
 }
