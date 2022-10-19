1b95b02d629a8e1906a75db5cdb3343df9d0ad5d
Junaid <Junaid.mah@hotmail.com>
2022-10-19 01:14:45 +0100

    Number of elements test added

--- src/java/test/java/Task1Test.java
+++ src/java/test/java/Task1Test.java
@@ -8,13 +8,18 @@ public class Task1Test {
 
     @Test
     public void minimumValue() {
-        assertEquals(-2, task1.sorting(new int[] {6, 9, 15, -2, 92, 11}));
-        assertEquals(-6, task1.sorting(new int[] {-6, 9, 15, -2, 92, 11}));
+        assertEquals(-2, task1.minVal(new int[] {6, 9, 15, -2, 92, 11}));
+        assertEquals(-6, task1.minVal(new int[] {-6, 9, 15, -2, 92, 11}));
     }
 
     @Test
     public void maxValue() {
-        assertEquals(92, task1.sorting(new int[] {6, 9, 15, -2, 92, 11}));
+        assertEquals(92, task1.maxVal(new int[] {6, 9, 15, -2, 92, 11}));
+    }
+
+    @Test
+    public void numberOfElements() {
+        assertEquals(6, task1.numberOfElements(new int[] {6, 9, 15, -2, 92, 11}));
     }
 
 
